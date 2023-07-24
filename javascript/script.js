f9382510930cd5f4c94596ffbd6d9ea910711d9e
Emzy <132276370+Emma-Halliwell@users.noreply.github.com>
2023-07-24 16:10:10 +0100

    JS changes with updated JS image

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,11 @@
-function changeColor() {
-    document.getElementById('title').style.color = "red";
+const randomColor = () => {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
+
+let changeColor = () => {
+    document.getElementById('title').style.color = randomColor();
 }
